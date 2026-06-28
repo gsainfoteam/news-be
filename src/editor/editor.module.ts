bb9bf39dc798e50a113ba1d1d09913478bbcd2a0
Dohyun Kim <dohyun06b@gmail.com>
2026-06-28 22:58:35 +0900

    fix: add EditorGuard to EditorModule providers

--- src/editor/editor.module.ts
+++ src/editor/editor.module.ts
@@ -3,11 +3,12 @@ import { EditorController } from './editor.controller';
 import { EditorService } from './editor.service';
 import { DrizzleModule } from '@lib/drizzle';
 import { EditorRepository } from './editor.repository';
+import { EditorGuard } from './guard/editor.guard';
 
 @Module({
   imports: [DrizzleModule],
   controllers: [EditorController],
-  providers: [EditorService, EditorRepository],
+  providers: [EditorService, EditorRepository, EditorGuard],
   exports: [EditorService],
 })
 export class EditorModule {}
