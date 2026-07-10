9537ff62cd80260ac9362d842a9fdf205e20fd4b
Dohyun Kim <dohyun06b@gmail.com>
2026-07-10 13:07:17 +0900

    feat: add PostgresExceptionFilter for handling database errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
@@ -6,6 +7,7 @@ import { UserModule } from './user/user.module';
 import { LoggerModule } from '@lib/logger';
 import { EditorModule } from './editor/editor.module';
 import { ArticleModule } from './article/article.module';
+import { PostgresExceptionFilter } from '@lib/drizzle';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { ArticleModule } from './article/article.module';
     EditorModule,
     ArticleModule,
     HealthModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: PostgresExceptionFilter,
+    },
   ],
 })
 export class AppModule {}
